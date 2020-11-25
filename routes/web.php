95dea49094e812e2c0e94e3c2e437488250006c6
Shubh Sheth <shubhsheth8@gmail.com>
2020-11-24 20:13:21 -0600

    Routes using Controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', 'App\Http\Controllers\Dashboard@index')->name('dashboard');
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', 'App\Http\Controllers\Dashboard@view')->name('dashboard');
+// Route::get('/test', 'App\Http\Controllers\Index@index');
